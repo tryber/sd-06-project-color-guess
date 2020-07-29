d7fb3436ee1d07369df44b21c9c0b4b495867259
Bruna Franciele <brunafranciele1902@gmail.com>
2020-07-29 16:14:08 -0300

    primeiro envio

---
+++ .github/script.js
@@ -0,0 +1,11 @@
+// criando os números aleatórios da cor que deverá ser adivinhada
+    let first = Math.round(Math.random() * 255);
+    let second = Math.round(Math.random() * 255);
+    let third = Math.round(Math.random() * 255);
+    let color = ("rgb(" + ([first, second, third].toString())) + (')');
+    let guess = ("(" + ([first, second, third].toString())) + (')');
+// adicionando texto da color no h2
+    let guessTheColor = document.querySelector("#rgb-color")
+    guessTheColor.innerHTML = guess
+
+ 
