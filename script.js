fa740c8912f17a4ef453fe0d620794885d657183
DehSartoreto <fixsartoreto@gmail.com>
2020-07-29 18:44:38 -0300

    add a verificação do acerto

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ window.onload = function() {
         rgb.push(b);
     }
 
+    function setCorrectColor() {
+        
+        
+    }
+
     generateColor();
     function starting() {
         let randomColor = document.querySelector("#rgb-color");
@@ -26,9 +31,13 @@ window.onload = function() {
                 let colorBall = "rgb(" + rgb + ")"
                 getBalls[i].style.backgroundColor = colorBall;
             }
+            let ballRandom = Math.floor(Math.random() * 6);
+            let sortedColor = "rgb" + document.querySelector("#rgb-color").innerHTML;
+            getBalls[ballRandom].style.backgroundColor = sortedColor;
         }
 
         setBallsColor();
+        document.querySelector("#answer").innerHTML = "Escolha uma cor";
 
     }
 
@@ -36,5 +45,73 @@ window.onload = function() {
 
     let restart = document.querySelector("#reset-game");
     restart.addEventListener("click", starting)
+
+    let ball01 = document.querySelector(".bola1");
+    let ball02 = document.querySelector(".bola2");
+    let ball03 = document.querySelector(".bola3");
+    let ball04 = document.querySelector(".bola4");
+    let ball05 = document.querySelector(".bola5");
+    let ball06 = document.querySelector(".bola6");
+
+    ball01.addEventListener("click", function() {
+        let corBola = ball01.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
+    ball02.addEventListener("click", function() {
+        let corBola = ball02.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
+    ball03.addEventListener("click", function() {
+        let corBola = ball03.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
+    ball04.addEventListener("click", function() {
+        let corBola = ball04.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
+    ball05.addEventListener("click", function() {
+        let corBola = ball05.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
+    ball06.addEventListener("click", function() {
+        let corBola = ball06.style.backgroundColor.replace(/\s/g, '')
+        let corTexto = "rgb" + document.querySelector("#rgb-color").innerHTML;
+        if (corBola !== corTexto){
+            document.querySelector("#answer").innerHTML = "Errou! Tente novamente!";
+        } else {
+            document.querySelector("#answer").innerHTML = "Acertou!";
+        }
+    })
+
     
 }
