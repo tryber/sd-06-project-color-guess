5ac689ded5332d396f2722ea4fb9561cf520af62
higoramorim <1810042@aluno.univesp.br>
2020-07-29 13:41:11 -0300

    add button funcionalidade insert escolha uma cor

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//
+const loadAnswer = document.getElementById("answer");
+function funcLoadAnswer(){
+    loadAnswer.style.marginLeft = '200px';
+    loadAnswer.style.marginTop = '20px';
+    loadAnswer.innerText = 'Escolha uma cor';
+}
+
+window.onload = function(){
+    funcLoadAnswer()
+}
