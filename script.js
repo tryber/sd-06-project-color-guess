4d78d464c347ead3ec897bdd6882d2238f426f47
higoramorim <1810042@aluno.univesp.br>
2020-07-29 17:06:02 -0300

    js working

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-const loadAnswer = document.getElementById("answer");
-function funcLoadAnswer(){
-    loadAnswer.style.marginLeft = '200px';
-    loadAnswer.style.marginTop = '20px';
-    loadAnswer.innerText = 'Escolha uma cor';
+const loadAnswer = document.getElementById('answer');
+function funcLoadAnswer() {
+  loadAnswer.style.marginLeft = '200px';
+  loadAnswer.style.marginTop = '20px';
+  loadAnswer.innerText = 'Escolha uma cor';
+}
+
+
+function makeRandomNumber(){
+  return Math.ceil(Math.random()*255);
+}
+
+function makeRandomColor(){
+  const R = makeRandomNumber();
+  const G = makeRandomNumber();
+  const B = makeRandomNumber();
+  const RGB = `rgb(${R}, ${G}, ${B})`;
+  return RGB;
 }
 
 const getBallsClass = document.querySelectorAll('.ball');
-
-window.onload = function(){
-    funcLoadAnswer()
+function changeColorBGBalls(){
+for (let index = 0; index < getBallsClass.length; index++) {
+    getBallsClass[index].style.backgroundColor.makeRandomColor();
+    
 }
+}
+
+window.onload = function() {
+  funcLoadAnswer();
+  changeColorBGBalls();
+};
+
