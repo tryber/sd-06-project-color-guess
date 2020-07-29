e30346e52736747359737357e47abd6ab491c76f
jederson <jmacedo.ti@gmail.com>
2020-07-29 18:00:32 -0300

    requirement 03

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-window.onload = function() {
+//window.onload = function() {
   const rgbText = document.querySelector('#rgb-color');
   //const text = generateRandomColor();
   rgbText.innerHTML = generateRandomColor();
+//}
+const ballsContainer = document.querySelector('.balls-container');
+
+function generateBalls(){
+  const randomNum = Math.floor(Math.random() * 6);
+  console.log(randomNum);
+  for (let i = 0 ; i < 6 ; i += 1){
+    let divBall = document.createElement('div');
+    divBall.classList.add('ball');
+    if (i === randomNum){
+      divBall.style.backgroundColor = `rgb${rgbText.innerHTML}`;
+    }else {
+      divBall.style.backgroundColor = `rgb${generateRandomColor()}`;
+    }
+    ballsContainer.appendChild(divBall);
+
+  }
+
 }
+generateBalls();
 
 function generateRandomColor(){
   const red = Math.floor(Math.random() * 255);
@@ -15,4 +34,3 @@ function generateRandomColor(){
 }
 
 
-console.log(generateRandomColor());
