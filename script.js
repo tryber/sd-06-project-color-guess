2f6ff4cf0dee0297241708a128039cfad22e9fb1
ricknois <rickssti@outlook.com>
2020-07-30 20:33:04 -0300

    Code Climate chatão

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-window.onload = function name() {
-  randomColors();
-  colorToMatch();
-  createBalls();
-}
-
 const showColor = document.querySelector('#rgb-color');
 const ballsList = document.querySelector('#create-balls');
 const avaliator = document.querySelector('#answer');
@@ -14,24 +8,29 @@ let colorToMatchStr2 = '';
 let colors = [];
 let pontos = 0;
 let total = 0;
+const grade = 3;
+const string1 = 'Pontuação: ';
+const string2 = 'rgb';
+const string3 = 'background-color: rgb';
+const string4 = ';';
+
 
 function randomColors() {
-  let j = 0; 
-  
+  let j = 0;
+
   while (j < 6) {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    j += 1
-    colors.push(`(${r}, ${g}, ${b})`) ;
+    j += 1;
+    colors.push(`(${r}, ${g}, ${b})`);
   }
 }
 
 function colorToMatch() {
   colorToMatchStr = colors[Math.floor(Math.random() * 6)]
   showColor.innerHTML = colorToMatchStr;
-  colorToMatchStr2 = 'rgb'+colorToMatchStr;
-  console.log(colorToMatchStr);
+  colorToMatchStr2 = string2 + colorToMatchStr;
 }
 
 function createBalls() {
@@ -39,39 +38,43 @@ function createBalls() {
   while (i < 6) {
     const create = document.createElement('div');
     create.setAttribute('class', 'ball');
-    create.setAttribute('style', 'background-color: rgb' + colors[i] + ';')
+    create.setAttribute('style', string3 + colors[i] + string4);
     ballsList.appendChild(create);
     i += 1;
-  } 
+  }
 }
 
- ballsList.addEventListener('click', function (event) {
+ballsList.addEventListener('click', function (event) {
   if (event.target.style.backgroundColor === colorToMatchStr2) {
-    avaliator.innerHTML = "Acertou!";
+    avaliator.innerHTML = 'Acertou!';
     score();
 
-  }else{
-    avaliator.innerHTML = "Errou! Tente novamente!";
+  } else {
+    avaliator.innerHTML = 'Errou! Tente novamente!';
   }
- })
+});
 
-  resetBtn.addEventListener('click', function () {
-    showColor.innerHTML = "";
-    colors = [];
-    let index = 0;
-    while (index < 6) {
-      const rmvBalls = document.querySelector('.ball')
-      index += 1;
-      ballsList.removeChild(rmvBalls)
-    }
-    randomColors();
-    colorToMatch();
-    createBalls();
-  })
-
-  function score() {
-    total = pontos += 3;
-    scoreShow.innerHTML = 'Pontuação:' + total;
+resetBtn.addEventListener('click', function () {
+  showColor.innerHTML = '';
+  colors = [];
+  let index = 0;
+  while (index < 6) {
+    const rmvBalls = document.querySelector('.ball');
+    index += 1;
+    ballsList.removeChild(rmvBalls);
   }
+  randomColors();
+  colorToMatch();
+  createBalls();
+})
 
+function score() {
+  total = pontos += grade;
+  scoreShow.innerHTML = string1 + total;
+}
 
+window.onload = function name() {
+  randomColors();
+  colorToMatch();
+  createBalls();
+}
