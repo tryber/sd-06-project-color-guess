8e2fd775353822ad7418f9065d2e8175e6e0cfa9
Marxneves <marxkhristie@gmail.com>
2020-07-30 13:16:59 -0300

    Inseri as cores aleatorias e fiz o layout basico

---
+++ script.js
@@ -0,0 +1,25 @@
+const rgbText = document.getElementById('rgb-color');
+const score = document.getElementById('score');
+const answer = document.getElementById('answer');
+const btnDificuldade = document.getElementById('dificuldade');
+const reset = document.getElementById('reset');
+const balls = document.querySelectorAll('.ball');
+
+function gerarCor() {
+  const red = Math.floor(Math.random()*255);
+  const blue = Math.floor(Math.random()*255);
+  const green = Math.floor(Math.random()*255);
+
+  return `(${red}, ${blue}, ${green})`;
+}
+
+for (let i = 0; i < balls.length; i += 1) {
+  balls[i].style.backgroundColor = 'rgb'+ gerarCor();
+}
+const number = Math.floor(Math.random()*6);
+const color = balls[number].style.backgroundColor;
+let text = '';
+for (let i = 3; i< color.length; i += 1) {
+  text += color[i]; 
+}
+rgbText.innerHTML = text;
