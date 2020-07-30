9e97699cf4b088014f28f09a339652a6e3d0d891
fefarina <wfelipefarina@gmail.com>
2020-07-30 17:16:05 -0300

    Update all files

---
+++ script.js
@@ -0,0 +1,44 @@
+let buttonReset = document.querySelector('#reset-game')
+let textRgb = document.querySelector('#rgb-color');
+let balls = document.querySelectorAll('.ball');
+let answer = document.querySelector('#answer');
+
+randomColor = () => {
+    function c() {
+        let hex = Math.floor(Math.random() * 256).toString(16);
+        return ("0"+String(hex)).substr(-2);
+    }
+    for(let i = 0; i < balls.length; i++) {
+        balls[i].style.backgroundColor = '#'+c()+c()+c();
+    }
+}
+
+window.onload = randomColor();
+
+buttonReset.addEventListener('click', randomColor)
+
+
+aleatoryRgb = () => {
+    for (let i = 0; i < balls.length; i++) {
+        textRgb.innerHTML = balls[i].style.backgroundColor;
+    }
+}
+
+window.onload = aleatoryRgb();
+buttonReset.addEventListener('click', aleatoryRgb)
+
+
+checkingColor = () => {
+    for(let i = 0; i < balls.length; i++) {
+        balls[i].addEventListener('click', function() {
+            if(textRgb.innerHTML === balls[i].style.backgroundColor) {
+                answer.innerText = "Você acertou!"
+            } else {
+                answer.innerText = "Tente novamente"
+            }
+        })
+    }
+}
+
+checkingColor();
+
