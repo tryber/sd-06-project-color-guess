0e9a115bc944b3720c3a4a9bd47fe24ae93278be
Marxneves <marxkhristie@gmail.com>
2020-07-30 14:11:24 -0300

    colocando botao reset e verificando funcionamento.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rgbText = document.getElementById('rgb-color');
 // const score = document.getElementById('score');
 const answer = document.getElementById('answer');
 // const btnDificuldade = document.getElementById('dificuldade');
-// const reset = document.getElementById('reset');
+const reset = document.getElementById('reset-game');
 const divBalls = document.getElementById('container-balls');
 const balls = document.querySelectorAll('.ball');
 
@@ -31,6 +31,20 @@ divBalls.addEventListener('click', (e) => {
   if (escolhido.style.backgroundColor === rgbCorreto) {
     answer.innerHTML = 'Acertou!';
   } else {
-    answer.innerHTML = 'Errou!';
+    answer.innerHTML = 'Errou! Tente novamente!';
   }
+});
+
+reset.addEventListener('click', () => {
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].style.backgroundColor = (`rgb${gerarCor()}`);
+  }
+  const number = Math.floor(Math.random() * 6);
+  const color = balls[number].style.backgroundColor;
+  let text = '';
+  for (let i = 3; i < color.length; i += 1) {
+    text += color[i];
+  }
+  rgbText.innerHTML = text;
+  answer.innerHTML = 'Escolha uma cor';
 });
