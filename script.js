f2d47519f50b37d7fe82b56fcd0aa8ac7bf4cc92
cyranowebdev <cyrano@cyranowebdev.com>
2020-07-30 18:55:09 -0300

    primeira tentativa com requisitos obrigatórios.

---
+++ script.js
@@ -0,0 +1,62 @@
+const balls = document.querySelectorAll('#balls .ball');
+const pickedColorElement = document.querySelector('#rgb-color');
+let answerRGB = '';
+let randomBallRGB = '';
+let rgbRandom = '';
+const resultElement = document.querySelector('#answer');
+const hand = document.querySelector('#hand');
+
+function genColors() {
+  let rgbColor1 = (Math.round(Math.random() * 255));
+  let rgbColor2 = (Math.round(Math.random() * 255));
+  let rgbColor3 = (Math.round(Math.random() * 255));
+  rgbRandom = '(' + rgbColor1 + ', ' + rgbColor2 + ', ' + rgbColor3 + ')';
+}
+
+function paintBalls() {
+  for (let key = 0; key < balls.length; key += 1) {
+    genColors();
+    balls.item(key).style.backgroundColor = 'rgb' + rgbRandom;
+    for (let checkColor = 0; checkColor < key; checkColor += 1) {
+      if (balls.item(key).style.backgroundColor = 'rgb' + rgbRandom) {
+        genColors();
+        balls.item(key).style.backgroundColor = 'rgb' + rgbRandom;
+      }
+    }
+  }
+}
+
+paintBalls();
+
+function pickRGB() {
+  let randomBall = Math.floor(Math.random() * 6);
+  randomBallRGB = balls.item(randomBall).style.backgroundColor;
+  answerRGB = randomBallRGB.substr(3);
+  pickedColorElement.innerHTML = answerRGB;
+}
+
+pickRGB();
+
+function resetGame(event) {
+  paintBalls();
+  pickRGB();
+  resultElement.innerText = 'Escolha uma cor';
+  hand.className = 'far fa-hand-point-down hands';
+}
+
+const resetBtn = document.querySelector('#reset');
+resetBtn.addEventListener('click', resetGame);
+
+function checkAnswer(event) {
+  if (event.target.style.backgroundColor === randomBallRGB) {
+    resultElement.innerText = 'Acertou!';
+    hand.className = 'far fa-thumbs-up hands';
+  } else {
+    resultElement.innerText = 'Errou! Tente novamente!';
+    hand.className = 'fas fa-poo hands';
+  }
+}
+
+for (let key = 0; key < balls.length; key += 1) {
+  balls.item(key).addEventListener('click', checkAnswer);
+}
