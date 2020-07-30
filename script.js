41a35672fa795a9c65a6af49394c95ca6ec589e9
Luiz Mello - Turma 06 <luizmacmillan@gmail.com>
2020-07-30 17:23:22 -0300

    Criada função para gerar as cores aleatóriamente e criado botão para reiniciar o jogo conforme etapas 5 e 6.

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
 const colorOptions = document.querySelector('.color-options');
-const correctColor = `rgb${document.querySelector('#rgb-color').innerText}`;
+const correctColor = document.querySelector('#rgb-color');
 let answerText = document.querySelector('#answer');
 
 colorOptions.addEventListener('click', function () {
   const selectedColor = event.target;
   const rgbOfSelectedColor = window.getComputedStyle(selectedColor, null).getPropertyValue('background-color');
 
-  console.log(rgbOfSelectedColor);
-  if (rgbOfSelectedColor === correctColor) {
+  if (rgbOfSelectedColor.substr(3) === correctColor.innerText) {
     answerText.innerText = 'Acertou!';
   } else {
     answerText.innerText = 'Errou! Tente novamente!';
   }
-})
+});
+
+function colorParam() {
+  const color = Math.floor(Math.random() * 300);
+  return color;
+}
+
+function getRandomColor() {
+  const newRgbRng = `rgb(${colorParam()}, ${colorParam()}, ${colorParam()})`;
+  return newRgbRng;
+};
+
+function colorAssigner() {
+  const colorsToSet = document.querySelector('.color-options').children;
+
+  for (let x = 0; x < colorsToSet.length; x += 1) {
+    colorsToSet[x].style.backgroundColor = getRandomColor();
+  }
+
+  function pickingTheRight() {
+    let pickedNumber = Math.floor(Math.random() * 6);
+    let pickedColor = colorsToSet[pickedNumber].style.backgroundColor;
+    return pickedColor.substr(3);
+  }
+
+  answerText.innerText = 'Escolha uma cor';
+  correctColor.innerText = pickingTheRight();
+};
+
+colorAssigner();
+
+const resetButton = document.querySelector('#reset-game');
+
+resetButton.addEventListener('click', colorAssigner);
