c9e0172d6c6a48d8b9ed906109f9923f5fedf4cb
luiz <phellipe501@gmail.com>
2020-07-30 20:24:07 -0300

    finalizado git add .

--- script.js
+++ script.js
@@ -3,21 +3,27 @@ window.onload = function(){
     let bolas = document.querySelector(".bolas");
     let gerada = document.querySelector("#rgb-color")
     let answer = document.querySelector("#answer")
-let reset = document.querySelector("#reset-game");
-    gerada.innerText = "rgb("+ (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + Math.floor(Math.random() * 256) + ")" ;
-
-
+    let reset = document.querySelector("#reset-game");
+    let score = document.querySelector("#score");
+    let randonN = Math.floor(Math.random() * 6);
+let scoreInt = 0;
+    
+    
     for(let i = 0; i < bola.length; i++){
         bola[i].style.backgroundColor = "rgb("+ (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + Math.floor(Math.random() * 256) + ")" ;
-
-      
     }
 
+  gerada.innerText = bola[randonN].style.backgroundColor
+
+
+ 
 
     bolas.addEventListener("click", function(event){
        var corSelect = event.target.style.backgroundColor
        if(corSelect == gerada.innerText){
-        answer.innerText = "Acertou!";
+        answer.innerHTML = "Acertou!";
+        scoreInt += 3;
+        score.innerText = scoreInt
        }else{
         answer.innerText = "Errou! Tente novamente!";
        }
@@ -33,6 +39,8 @@ let reset = document.querySelector("#reset-game");
         }
         answer.innerText = "Escolha uma cor";
         gerada.innerText = "rgb("+ (Math.floor(Math.random() * 256)) + ", " + (Math.floor(Math.random() * 256)) + ", " + Math.floor(Math.random() * 256) + ")" ;
+        let randonN = Math.floor(Math.random() * 6);
+        gerada.innerText = bola[randonN].style.backgroundColor
     })
 
 
