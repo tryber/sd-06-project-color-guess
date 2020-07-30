63a95a9df108661ffeb2fba887ec66bca51edf6a
AlexanderFeitosa <alexander.feitosa@gmail.com>
2020-07-30 16:51:37 -0300

    Bonus Task

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let score = 0;
+
 function ballGenerate() {
   const newBall = document.createElement('div');
   newBall.className = 'ball';
@@ -12,12 +14,12 @@ function generateChallenge() {
 }
 
 function generateAnswers() {
-  var answerArray = [];
+  let answerArray = [];
   answerArray.push(`rgb${document.getElementById('rgb-color').innerText}`);
   for (let i = 1; i < document.querySelectorAll('.ball').length; i += 1) {
-    let newRed = Math.ceil(Math.random() * 255);
-    let newGreen = Math.ceil(Math.random() * 255);
-    let newBlue = Math.ceil(Math.random() * 255);
+    const newRed = Math.ceil(Math.random() * 255);
+    const newGreen = Math.ceil(Math.random() * 255);
+    const newBlue = Math.ceil(Math.random() * 255);
     answerArray.push(`rgb(${newRed}, ${newGreen}, ${newBlue})`);
   }
   for (let j = 0; j < document.querySelectorAll('.ball').length; j += 1) {
@@ -27,17 +29,21 @@ function generateAnswers() {
   }
 }
 
-for (i = 0; i < 6; i += 1) {
+for (let i = 0; i < 6; i += 1) {
   ballGenerate();
 }
 
 generateChallenge();
 generateAnswers();
 
+document.getElementById('score').innerText = score;
+
 for (let i = 0; i < document.querySelectorAll('.ball').length; i += 1) {
   document.querySelectorAll('.ball')[i].addEventListener('click', function () {
-    if (document.querySelectorAll('.ball')[i].style.backgroundColor === 'rgb' + document.querySelector('#rgb-color').innerText) {
+    if (document.querySelectorAll('.ball')[i].style.backgroundColor === `rgb${document.querySelector("#rgb-color").innerText}`) {
       document.querySelector('#answer').innerText = 'Acertou!';
+      score += 3;
+      document.getElementById('score').innerText = score;
     } else {
       document.querySelector('#answer').innerText = 'Errou! Tente novamente!';
     }
