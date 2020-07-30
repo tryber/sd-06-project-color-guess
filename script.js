140bd6f3084a921d06c4da864cabdda98e41df04
Rods <rods.leite27@gmail.com>
2020-07-30 16:31:09 -0300

    Requisitos 1 2 3 e 5

---
+++ script.js
@@ -0,0 +1,14 @@
+const balls = document.getElementsByClassName('ball')
+const divBalls = document.getElementsByClassName('div-balls')
+
+let pontuacao = 0;
+function rgbAleatorio() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `(${red}, ${green}, ${blue})`;
+}
+
+for (let i = 0; i < balls.length; i+= 1) {
+  balls[i].style.background = (`rgb${rgbAleatorio()}`);
+}
