72e5e593b3d4b77919bb05fe8dfc862f08955f20
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-07-29 22:18:52 -0300

    Implementado captura da cor ao clicar no círculo

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 window.onload = function () {
 
   coloringCircles();
+  const ballsPalette = document.querySelector('#balls');
+
+  function randomNumbers() {
+    return Math.floor(Math.random() * 255)
+  }
 
   function generatorRandomColors() {
-    let randomGeneratorNumber = Math.floor(0x100000000) * Math.random();
-    let formatatterRgb =  '('  + (randomGeneratorNumber & 255) + ', ' + (randomGeneratorNumber >> 8 & 255) + ', ' + (randomGeneratorNumber >> 16 & 255) + ')';
-    return formatatterRgb
+    let generatedRgb = '(' + randomNumbers() + ", " + randomNumbers() + ", " + randomNumbers() + ')';
+    return generatedRgb;
   }
   
   function coloringCircles() {
     const circles = document.getElementsByClassName('ball');
     const guessing = document.getElementById('rgb-color')
     for (let i = 0; i < circles.length; i += 1) {
-      circles[i].style.backgroundColor = "rgb"+generatorRandomColors();
+      circles[i].style.backgroundColor = 'rgb'+generatorRandomColors();
       guessing.innerHTML = generatorRandomColors();
     }
+  }
+  const resetButton = document.getElementById('reset-game');
+  resetButton.addEventListener('click', coloringCircles);
 }
-const resetButton = document.getElementById('reset-game');
-resetButton.addEventListener('click', coloringCircles);
+
+// Clicar e pegar a cor
+
+function chooseBall(event) {
+  document.querySelector('.selected').classList.remove('selected');
+  event.target.classList.add('selected');
+  let selectedColor = document.querySelector('.selected').style.backgroundColor;
+  console.log(selectedColor)
 }
+
+ballsPalette.addEventListener('click', chooseBall);
+
+
+
+
 
