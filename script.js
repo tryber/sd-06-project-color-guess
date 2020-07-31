10d312d04f6d3645485dc2ef5992efb97acbd331
Elcio Rodrigo <e.rhodrigo.a@gmail.com>
2020-07-31 13:27:32 -0300

    passo 5

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const randomColor0 = '#' + ((1<<24) * Math.random()|0).toString(16);
-const randomColor1 = '#' + ((1<<24) * Math.random()|0).toString(16);
-const randomColor2 = '#' + ((1<<24) * Math.random()|0).toString(16);
-const randomColor3 = '#' + ((1<<24) * Math.random()|0).toString(16);
-const randomColor4 = '#' + ((1<<24) * Math.random()|0).toString(16);
-const randomColor5 = '#' + ((1<<24) * Math.random()|0).toString(16);
+const randomColor0 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+const randomColor1 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+const randomColor2 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+const randomColor3 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+const randomColor4 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+const randomColor5 = '#' + ((1 << 24) * Math.random()|0).toString(16);
 let balls = document.querySelectorAll('.ball');
 balls[0].style.backgroundColor = randomColor0;
 balls[1].style.backgroundColor = randomColor1;
@@ -12,26 +12,25 @@ balls[3].style.backgroundColor = randomColor3;
 balls[4].style.backgroundColor = randomColor4;
 balls[5].style.backgroundColor = randomColor5;
 function hexToRgb(randomColor4) {
-  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(randomColor4);
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(randomColor4);
   return result ? {
     r: parseInt(result[1], 16),
     g: parseInt(result[2], 16),
-    b: parseInt(result[3], 16)
+    b: parseInt(result[3], 16),
   } : null;
 }
 function corInicial() {
-  let showColor = document.getElementById('rgb-color');
+  const showColor = document.getElementById('rgb-color');
   showColor.innerHTML = `(${hexToRgb(randomColor4).r}, ${hexToRgb(randomColor4).g}, ${hexToRgb(randomColor4).b})`;
 }
 corInicial();
-let acertos =0;
+let acertos = 0;
 function bolaCerta() {
   return document.getElementById('answer').innerHTML = 'Acertou!', document.getElementById('score').innerHTML = `Placar: ${acertos += 3}`;
-
 }
 balls[4].addEventListener('click', bolaCerta);
 function bolaErrada() {
-  return document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+  return document.getElementById('answer').innerHTML = 'Errou! Tente novamente!';
 }
 balls[0].addEventListener('click', bolaErrada);
 balls[1].addEventListener('click', bolaErrada);
@@ -39,12 +38,12 @@ balls[2].addEventListener('click', bolaErrada);
 balls[3].addEventListener('click', bolaErrada);
 balls[5].addEventListener('click', bolaErrada);
 function reset() {
-  const randomColor0 = '#' + ((1<<24) * Math.random()|0).toString(16);
-  const randomColor1 = '#' + ((1<<24) * Math.random()|0).toString(16);
-  const randomColor2 = '#' + ((1<<24) * Math.random()|0).toString(16);
-  const randomColor3 = '#' + ((1<<24) * Math.random()|0).toString(16);
-  const randomColor4 = '#' + ((1<<24) * Math.random()|0).toString(16);
-  const randomColor5 = '#' + ((1<<24) * Math.random()|0).toString(16);
+  const randomColor0 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+  const randomColor1 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+  const randomColor2 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+  const randomColor3 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+  const randomColor4 = '#' + ((1 << 24) * Math.random()|0).toString(16);
+  const randomColor5 = '#' + ((1 << 24) * Math.random()|0).toString(16);
   let balls = document.querySelectorAll('.ball');
   balls[0].style.backgroundColor = randomColor0;
   balls[1].style.backgroundColor = randomColor1;
@@ -52,10 +51,9 @@ function reset() {
   balls[3].style.backgroundColor = randomColor3;
   balls[4].style.backgroundColor = randomColor4;
   balls[5].style.backgroundColor = randomColor5;
-  let showColor = document.getElementById('rgb-color');
+  const showColor = document.getElementById('rgb-color');
   showColor.innerHTML = `(${hexToRgb(randomColor4).r}, ${hexToRgb(randomColor4).g}, ${hexToRgb(randomColor4).b})`;
   document.getElementById('answer').innerHTML = 'Escolha uma cor';
 }
 let btn = document.getElementById('reset-game');
-btn.addEventListener('click', reset)
-
+btn.addEventListener('click', reset);
