3f8b1862d272b5f43ef32833d0a7717e4ca77047
Dilenio Enderle <dilenio@gmail.com>
2020-07-31 09:30:25 -0300

    6 - Crie um botão para iniciar/reiniciar o jogo

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Criação das bolas coloridas
 let colors;
 let count = 0;
-let colorToGuess = 'rgb(0, 0, 0)';
+let score = 0;
+let colorToGuess;
 const answer = document.querySelector('#answer');
-answer.innerHTML = 'Escolha uma cor';
 
 function createColorBalls() {
   const balls = document.querySelector('#balls');
@@ -31,10 +31,11 @@ function generateRandomColor() {
 
 // Cria as 6 bolas de cores
 function generateColors() {
-  colors = [];
+  let colorsTo = [];
   for (let index = 0; index < 6; index += 1) {
-    colors.push(generateRandomColor());
+    colorsTo.push(generateRandomColor());
   }
+  return colorsTo;
 }
 
 // escolhe uma das cores geradas na paleta para adivinhar
@@ -43,6 +44,29 @@ function selectColorGuess() {
   return colorToGuess;
 }
 
+// Inicializa o jogo
+function initGame() {
+  // reseta paleta
+  const resetPalette = document.querySelector('#balls');
+  while (resetPalette.firstChild) {
+    resetPalette.removeChild(resetPalette.lastChild);
+  }
+  // gera paleta de cores
+  colors = generateColors();
+  // cria as bolas coloridas
+  createColorBalls();
+  // escolhe uma cor da paleta gerada
+  selectColorGuess();
+  const rgbColor = document.querySelector('#rgb-color');
+  rgbColor.innerHTML = colorToGuess;
+  answer.innerHTML = 'Escolha uma cor';
+  count = 0;
+};
+
+// Botão que chama a funcão para reinicializar o jogo
+const resetNow = document.querySelector('#reset-game');
+resetNow.addEventListener('click', initGame);
+
 const runNow = document.getElementById('balls');
 runNow.addEventListener('click', function (event) {
   const runner = event.target;
@@ -50,9 +74,13 @@ runNow.addEventListener('click', function (event) {
     if (runner.style.backgroundColor === colorToGuess) {
       answer.innerHTML = 'Acertou!';
       count += 1;
+      score += 3;
     } else {
       answer.innerHTML = 'Errou! Tente novamente!';
       count += 1;
+      if (score >= 0) {
+        score -= 1;
+      }
     }
   }
 });
@@ -61,11 +89,6 @@ runNow.addEventListener('click', function (event) {
 // });
 
 window.onload = function () {
-  // gera paleta de cores
-  generateColors();
-  // cria as bolas coloridas
-  createColorBalls();
-  // escolhe uma cor da paleta gerada
-  const rgbColor = document.querySelector('#rgb-color');
-  rgbColor.innerHTML = selectColorGuess().substring(3);
+  // Inicializa o jogo
+  initGame();
 };
