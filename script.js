2596e2253369bce97609b164f5b152bb73ae0da4
isabellavjs <isabellavjs@gmail.com>
2020-08-02 10:51:28 -0300

    Adding requirement 6

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-//Create a random rgb color
-let rgbColor = document.getElementById('rgb-color');
-function generateColor() {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    let colorResult = 'rgb(' + x + ',' + y + ',' + z + ')';
+window.onload = function() {
     
-    return colorResult;
-}
-rgbColor.innerHTML = generateColor();
-
-//Generate a random number from 0 to 5.
-function randomNumber(max) {
-    return (Math.floor(Math.random()*Math.floor(max)));
-}
-randomNumber(6);
-console.log(randomNumber(6));
-
-//Generate the balls' color, and define the correct answer
-let balls = document.querySelectorAll('.ball');
-
-function rgbColorBall() {
-    for (let index = 0; index < balls.length; index += 1) {
-        if (index === randomNumber(6)) {
-            balls[index].classList.add('correct-answer');
-            let rgbCorrectAnswer = rgbColor.innerHTML;
-            balls[index].style.backgroundColor = rgbCorrectAnswer;
-        } else {
-        let rgb = generateColor();
-        balls[index].style.backgroundColor = rgb;
+    //Create a random rgb color
+    let rgbColor = document.getElementById('rgb-color');
+    function generateColor() {
+        var x = Math.floor(Math.random() * 256);
+        var y = Math.floor(Math.random() * 256);
+        var z = Math.floor(Math.random() * 256);
+        let colorResult = 'rgb(' + x + ',' + y + ',' + z + ')';
+        
+        return colorResult;
+    }
+    rgbColor.innerHTML = generateColor();
+    
+    //Generate a random number from 0 to 5.
+    function randomNumber(max) {
+        return (Math.floor(Math.random()*Math.floor(max)));
+    }
+    randomNumber(6);
+    console.log(randomNumber(6));
+    
+    //Generate the balls' color, and define the correct answer
+    let balls = document.querySelectorAll('.ball');
+    
+    function rgbColorBall() {
+        for (let index = 0; index < balls.length; index += 1) {
+            if (index === randomNumber(6)) {
+                balls[index].classList.add('correct-answer');
+                let rgbCorrectAnswer = rgbColor.innerHTML;
+                balls[index].style.backgroundColor = rgbCorrectAnswer;
+            } else {
+            let rgb = generateColor();
+            balls[index].style.backgroundColor = rgb;
+            }
         }
     }
+    rgbColorBall();
 }
-rgbColorBall();
 
 //Ask the user to select a ball
 let gameText = document.createElement('p');
@@ -51,3 +54,11 @@ for (let index = 0; index < balls.length; index += 1) {
         }
     })
 }
+
+//Reset the game
+const resetButton = document.getElementById('reset-game');
+resetButton.addEventListener('click', function() {
+    rgbColor.innerHTML = generateColor();
+    rgbColorBall();
+    gameText.innerHTML = '';
+})
