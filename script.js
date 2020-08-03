b776c63efff9b70eb7429eb4c98bebb82664a669
Luiz E F Simoes <luizefsimoes@gmail.com>
2020-08-03 15:08:59 -0300

    Implementando a funcionalidade de placar

--- script.js
+++ script.js
@@ -28,20 +28,41 @@ window.onload = function () {
   resetButton.addEventListener('click', coloringCircles);
 }
 
+let score=0;
 function chooseBall(event) {
   document.querySelector('.selected').classList.remove('selected');
   event.target.classList.add('selected');
+  const scoreElement = document.querySelector('#score');//
   let selectedColor = document.querySelector('.selected').style.backgroundColor;
   const valorApresentado = document.getElementById('rgb-color').innerHTML;
   const textBoxResult = document.getElementById('answer');
-  if(valorApresentado === selectedColor) {
+    if(valorApresentado === selectedColor) {
     textBoxResult.innerHTML ="Acertou!";
+    score += 3;//
   } else {
     textBoxResult.innerHTML ="Errou! Tente novamente!";
+    score += 0;//
   }
+  scoreElement.innerHTML = "Placar: "+ score;
 }
 
 ballsPalette.addEventListener('click', chooseBall);
+
+// function verifyColor() {
+//   const selectedBackground = document.querySelector('#rgb-color').innerHTML;
+//   const scoreElement = document.querySelector('#score');
+//   let score = parseInt(scoreElement.innerHTML, 10);
+//   if (event.target.style.backgroundColor === selectedBackground) {
+//     const answerElement = document.querySelector('#answer');
+//     answerElement.innerHTML = 'Acertou!';
+//     score += 3;
+//   } else {
+//     const answerElement = document.querySelector('#answer');
+//     answerElement.innerHTML = 'Errou! Tente novamente!';
+//     score += 0;
+//   }
+//   scoreElement.innerHTML = score.toString();
+// },
 
 
 
