85f5e963c82965b95ed5804bd5809797f7d7fbf8
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-08-04 00:28:56 -0300

    Add reset button to restart the game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const rgbText = document.getElementById('rgb-color');
 const ballsCollections = document.getElementById('ball-collections');
+const btnResetGame = document.getElementById('reset-game');
 
 // Gera uma RGB aleatória;
 function randomColor() {
@@ -17,6 +18,7 @@ function createBall(color) {
   return ballsOptions;
 }
 
+// Cria as bolinhas e sorteia aleatoriamente uma das cores para o jogo;
 function createOptionsColors(options) {
   for (let index = 0; index < options.length; index += 1) {
     const balls = createBall(options[index]);
@@ -25,7 +27,7 @@ function createOptionsColors(options) {
   rgbText.innerText = options[Math.floor(Math.random() * 6)];
 }
 
-// 
+// Cria array de cores randomizadas;
 function generateColorOptions() {
   const colorsOptions = [];
   for (let index = 0; index < 6; index += 1) {
@@ -34,6 +36,23 @@ function generateColorOptions() {
   return createOptionsColors(colorsOptions);
 }
 
-window.onload = function () {
+// Mostra o resultado;
+function resultOfGame() {
+  const answer = document.getElementById('answer');
+  answer.innerHTML = 'Escolha uma cor';
+}
+
+// Reinicia o jogo com novas cores;
+function resetGame() {
+  const clearBalls = document.querySelectorAll('.ball');
+  for (let index = 0; index < clearBalls.length; index += 1) {
+    clearBalls[index].remove();
+  }
   generateColorOptions();
+}
+
+window.onload = function () {
+  btnResetGame.addEventListener('click', resetGame);
+  generateColorOptions();
+  resultOfGame();
 };
