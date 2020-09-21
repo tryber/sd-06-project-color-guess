ac94068c9c7ef83c1f5c2d32c1f031067d93fd51
Lais Gurgel <laisgurgelf@gmail.com>
2020-09-21 15:04:01 -0300

    Botão para iniciar/reiniar jogo

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+const btnResetGame = document.getElementById('reset-game');
+
 function randomColor() {
   const R = Math.floor(Math.random() * (255 + 1));
   const G = Math.floor(Math.random() * (255 + 1));
   const B = Math.floor(Math.random() * (255 + 1));
-
-  return `rgb(${R}, ${G}, ${B})`;
+  return `(${R}, ${G}, ${B})`;
 }
 
 function createBall(elements) {
   const ballContainer = document.getElementById('colors');
   for (let index = 0; index < elements; index += 1) {
     const ballElement = document.createElement('div');
+    const color = randomColor();
     ballElement.classList.add('ball');
-    ballElement.style.backgroundColor = randomColor();
+    ballElement.style.backgroundColor = `rgb${color}`;
     ballContainer.appendChild(ballElement);
   }
 }
+
+btnResetGame.addEventListener('click', () => {
+  const rgbColor = document.getElementById('rgb-color');
+  const answer = document.getElementById('answer');
+  rgbColor.innerText = randomColor();
+  answer.innerText = 'Escolha uma cor';
+});
 
 window.onload = function () {
   createBall(6);
