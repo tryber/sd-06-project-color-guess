433bcdf712ec345864d84914bb1a2fba0e6fdc85
Lais Gurgel <laisgurgelf@gmail.com>
2020-11-17 14:36:54 -0300

    Finishing project

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+let score = 0;
+let count = 0;
+let arrayOfColors = [];
+let colorToGuess = '';
 const btnResetGame = document.getElementById('reset-game');
+const rgbColor = document.getElementById('rgb-color');
+const scoreElement = document.getElementById('score');
+const ballContainer = document.getElementById('colors');
+const answer = document.getElementById('answer');
 
 function randomColor() {
   const R = Math.floor(Math.random() * (255 + 1));
@@ -8,23 +16,56 @@ function randomColor() {
 }
 
 function createBall(elements) {
-  const ballContainer = document.getElementById('colors');
+  arrayOfColors = [];
   for (let index = 0; index < elements; index += 1) {
     const ballElement = document.createElement('div');
     const color = randomColor();
+    arrayOfColors.push(color);
     ballElement.classList.add('ball');
     ballElement.style.backgroundColor = `rgb${color}`;
     ballContainer.appendChild(ballElement);
   }
 }
 
-btnResetGame.addEventListener('click', () => {
-  const rgbColor = document.getElementById('rgb-color');
-  const answer = document.getElementById('answer');
-  rgbColor.innerText = randomColor();
-  answer.innerText = 'Escolha uma cor';
+function getColorToGuess() {
+  const numberOfColors = arrayOfColors.length - 1;
+  const index = Math.ceil(Math.random() * numberOfColors);
+  colorToGuess = arrayOfColors[index];
+  return colorToGuess;
+}
+
+function updateScore() {
+  scoreElement.innerHTML = `Placar: ${score}`;
+}
+
+ballContainer.addEventListener('click', function ({ target }) {
+  if (target.className === 'ball' && count === 0) {
+    if (target.style.backgroundColor === `rgb${colorToGuess}`) {
+      answer.innerHTML = 'Acertou!';
+      count += 1;
+      score += 3;
+    } else {
+      answer.innerHTML = 'Errou! Tente novamente!';
+      count += 1;
+      if (score > 0) {
+        score -= 1;
+      }
+    }
+  }
+  updateScore();
 });
 
-window.onload = function () {
+function initGame() {
+  ballContainer.innerHTML = '';
   createBall(6);
+  rgbColor.innerText = getColorToGuess();
+  answer.innerText = 'Escolha uma cor';
+  count = 0;
+  updateScore();
+}
+
+btnResetGame.addEventListener('click', initGame);
+
+window.onload = function () {
+  initGame();
 };
